8a29ab1cb8cd72eaf8035f3b71dae397541c4ed9
Andrew Taylor <andrew.taylor@webappuk.com>
2014-09-18 12:33:47 +0100

    Added ngTools

---
+++ ngtools.js
@@ -0,0 +1,11 @@
+function inject(name) {
+	return angular
+		.element(document.querySelector('[ng-app],.ng-scope'))
+		.injector()
+		.get(name);
+}
+
+function scope(element) {
+	var e = angular.element(element);
+	return e.isolateScope() || e.scope();
+}
