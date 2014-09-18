2bd66c497ab9173c718231c52ad53d537ac96dea
Andrew Taylor <andrew.taylor@webappuk.com>
2014-09-18 17:37:58 +0100

    undid accidentally committed broken warp.

--- warp.js
+++ warp.js
@@ -4,19 +4,23 @@ var strength = strength ? strength * 2 : 0.05,
 	ivl,
 	c = document.getElementsByTagName('*');
 function fn() {
-	c.forEach(function(e) {
-		var m = 'matrix3d(',
+	for (var i = 0; i < c.length; ++i) {
+		var m = '',
+			e = c[i],
 			f = [e.offsetWidth, e.offsetHeight, 100, 1];
-		for (var x = 0; x < 4; ++x)
-			for (var y = 0; y < 4; ++y)
-				m += (m ? ', ' : '') +
-					((Math.random() * strength - strength / 2 ) / f[x] + (x == y));
-		var t = 'transform ' + time + 's';
+		for (var x = 0; x < 4; ++x) {
+			var t,
+				s = strength / f[x];
+			for(var y = 0; y < 4; ++y)
+				m += (m ? ', ' : '') + ((Math.random() * s - s / 2 ) + (x == y));
+		}
+		t = 'transform ' + time + 's';
 		e.style.transition = t;
 		e.style.mozTransition = '-moz-' + t;
 		e.style.webkitTransition = '-webkit-' + t;
-		e.style.transform = e.style.mozTransform = e.style.webkitTransform = m + ')';
-	});
+		e.style.transform = e.style.mozTransform = e.style.webkitTransform =
+			'matrix3d(' + m + ')';
+	}
 }
 if (ivl)
 	clearInterval(ivl);
